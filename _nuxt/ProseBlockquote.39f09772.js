5013edfa32f6894a3d349e38e2730e051ab2438e
mazadcse <mazadcse@users.noreply.github.com>
2023-08-04 09:19:54 +0000

    deploy: 5d06e34d25d814266a5b920ab0e63dfbf51323cf

--- _nuxt/ProseBlockquote.39f09772.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,o as t,d as r,X as c}from"./entry.96875049.js";const s={};function n(e,l){return t(),r("blockquote",null,[c(e.$slots,"default")])}const _=o(s,[["render",n]]);export{_ as default};
