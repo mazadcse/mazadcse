f0445b140c413fbff61bd351f5e1c8f76e6fd7a7
Mahedi Azad <mazadcse@gmail.com>
2023-08-04 18:27:44 +0600

    picks data

---
+++ data/picks.js
@@ -0,0 +1,12 @@
+[
+    {
+        "cover": "https://i.ytimg.com/vi/17zBODTpuoo/maxresdefault.jpg",
+        "title": "A First Look at Nuxt SERVER COMPONENTS",
+        "url": "https://www.youtube.com/watch?v=17zBODTpuoo"
+    },
+    {
+        "cover": "https://roe.dev/og/nuxt-server-components.jpg",
+        "title": "A guide to Nuxt server components",
+        "url": "https://roe.dev/blog/nuxt-server-components?ck_subscriber_id=2260814617"
+    }
+]
